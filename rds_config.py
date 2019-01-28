81bbfa486e539d2d8fbacb401cbc9b5a951ca08e
SWD Mahbubur Rahman <mahbubur@data-path.net>
2019-01-28 16:25:48 +0600

    Final

---
+++ rds_config.py
@@ -0,0 +1,5 @@
+#config file containing credentials for RDS MySQL instance
+db_endpoint="octopi-eye-rds.chceowt2kteg.ap-southeast-1.rds.amazonaws.com"
+db_username = "mahbub"
+db_password = "mahbub90"
+db_name = "codathon"
