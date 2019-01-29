9f975e2d8a4bfb4f274ccc7eadf409e91714e090
Mahbub <mahbub.apece@gmail.com>
2019-01-29 09:06:00 +0600

    Delete rds_config.py

--- rds_config.py
+++
@@ -1,5 +0,0 @@
-#config file containing credentials for RDS MySQL instance
-db_endpoint="octopi-eye-rds.chceowt2kteg.ap-southeast-1.rds.amazonaws.com"
-db_username = "mahbub"
-db_password = "mahbub90"
-db_name = "codathon"
